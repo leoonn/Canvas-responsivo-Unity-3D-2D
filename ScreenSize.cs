024eb490409401efc8e0043680053c158e1f2d4b
leoonn <flateia13@hotmail.com>
2021-07-18 13:59:25 -0300

    initial commit code

---
+++ ScreenSize.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSize : MonoBehaviour
+{
+    
+    private float orthoSize = 10f;
+    [SerializeField]
+    private float aspect = 1.75f;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         ScreenAjust();
+    }
+
+    void ScreenAjust()
+    {
+        Camera.main.projectionMatrix = Matrix4x4.Ortho(-orthoSize * aspect, orthoSize * aspect, -orthoSize, orthoSize, Camera.main.nearClipPlane , Camera.main.farClipPlane);
+    }
+}
